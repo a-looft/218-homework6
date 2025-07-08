4c712d8b90877a1fb5f449f52bb58f7238ea469f
a-looft <aidalooft@icloud.com>
2025-07-08 15:44:03 -0400

    added loggig config and integration w calculator

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from app.calculator_app import run
+from dotenv import load_dotenv
+import os
+import logging
+import logging.config
 
 if __name__ == "__main__":
     run()
+
+load_dotenv()
+env = os.getenv("ENVIRONMENT", "production")
+print(f"Running in {env} mode")
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger(__name__)
+logger.info("Starting calculator app")
